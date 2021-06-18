cd3bd0b7b9e09d9a7b4aefbf0ca91648fa856ba3
mg699 <42572104+Unknown807@users.noreply.github.com>
2021-06-18 18:41:11 +0100

    Going to use vega to plot data

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,35 @@
-const msg: string = "Hello World There!";
-console.log(msg);
+import { Config, TopLevelSpec, compile } from "vega-lite";
+import vegaEmbed from "vega-embed";
+
+let yourVlSpec: TopLevelSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'A simple bar chart with embedded data.',
+    data: {
+      values: [
+        {a: 'A', b: 28},
+        {a: 'B', b: 55},
+        {a: 'C', b: 43},
+        {a: 'D', b: 91},
+        {a: 'E', b: 81},
+        {a: 'F', b: 53},
+        {a: 'G', b: 19},
+        {a: 'H', b: 87},
+        {a: 'I', b: 52}
+      ]
+    },
+    mark: 'bar',
+    encoding: {
+      x: {field: 'a', type: 'ordinal'},
+      y: {field: 'b', type: 'quantitative'}
+    }
+  };
+
+const config: Config = {
+    bar: {
+        color: "firebrick"
+    }
+}
+
+const vegaSpec = compile(yourVlSpec, {config}).spec;
+
+vegaEmbed('#vis', vegaSpec);
